227043d599f93adb387cca2550ec6e31855a0630
Arman <hurianarmann@gmail.com>
2023-10-12 02:12:26 +0330

    add tailwind style & new template

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,20 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors : {
+        darkblue : "#232340",
+        darkColor : "#3C3C61",
+        blueBg : "#323259",
+      },
+      width: {
+        '100': '25rem',
+      },
       fontFamily:{
         'vazir' : ['vazir'],
         'vazirmatn' : ['vazirmatn'],
-        'yekan' : ['yekan']
+        'yekan' : ['yekan'],
+        'yekanbakh' : ['yekanbakh'],
+        'steretch' : ['steretch'],
       },
     },
   },
