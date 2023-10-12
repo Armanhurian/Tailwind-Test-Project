e692fc32a62cfe55a651b3b012c7d25ee5476bb0
Arman <hurianarmann@gmail.com>
2023-10-12 18:40:20 +0330

    Completed !

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,18 @@ module.exports = {
         darkblue : "#232340",
         darkColor : "#3C3C61",
         blueBg : "#323259",
+        skyBlue : "#EAE4FF",
+        darkWhite : "#F5F5F5",
+        dark : "#110E2D",
+        bgwhite : "#E8E8F4",
+        pink : "#E8DCFC"
       },
       width: {
         '100': '25rem',
+      },
+      spacing : {
+        "45" : "10.7rem;",
+        "100" : "29rem;",
       },
       fontFamily:{
         'vazir' : ['vazir'],
